702679ab21a697c9cf186836f8e39f2abd06477b
Jon <jonamar@gmail.com>
2022-06-01 19:55:15 +0000

    basic template and css all working w preview

--- www/index.php
+++ www/index.php
@@ -5,9 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.typekit.net/eiw7pxd.css">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="./styles/styles.css">
+    <title>Scrappy Kin</title>
 </head>
 <body>
+
+    <h1>Pigeon Perfect</h1>
     
 </body>
 </html>
