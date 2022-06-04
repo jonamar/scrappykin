d5c02c2329da42675c1b001ecf88f0bfd874d475
Jon <jonamar@gmail.com>
2022-06-04 21:38:49 +0000

    php templating, wip typography . bug: courier

--- www/index.php
+++ www/index.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.typekit.net/eiw7pxd.css">
-    <link rel="stylesheet" type="text/css" href="./styles/styles.css">
-    <title>Scrappy Kin</title>
-</head>
-<body>
+<?php
+$title = 'Scrappy Kin';
+include 'head.php';?>
 
     <h1>Pigeon Perfect</h1>
-    
-</body>
-</html>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas accusamus dolores, libero hic officia ut. Similique corporis vero eaque voluptatibus, quaerat sit amet ea, hic eius a deleniti totam! Velit?</p>
+
+<?php include 'foot.php';?>
