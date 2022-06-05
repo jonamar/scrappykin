891f16455b0377269c7e2c404465d14dd6412c25
Jon <jonamar@gmail.com>
2022-06-05 18:39:47 +0000

    normalize, reset. early base styles.

--- www/head.php
+++ www/head.php
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html class="no-js" lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.typekit.net/eiw7pxd.css">
-    <link rel="stylesheet" type="text/css" href="./styles/styles.css">
-    <title><?php echo $title ?></title>
+  <meta charset="utf-8">
+  <title><?php echo $title ?></title>
+  <meta name="description" content="A radially ethical stuffed animal company, obsessed with pigeons.">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+
+  <!-- 
+  TODO: Social open graph stuff  
+  <meta property="og:title" content="Scrappy Kin">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content=""> -->
+
+  <!-- 
+  TODO: create iecons  
+  <link rel="icon" href="/favicon.ico" sizes="any">
+  <link rel="icon" href="/icon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="icon.png"> -->
+  
+  <link rel="stylesheet" href="https://use.typekit.net/eiw7pxd.css">
+  <link rel="stylesheet" type="text/css" href="./styles/styles.css">
+  
+  <!-- HUH? 
+  <link rel="manifest" href="site.webmanifest">
+  <meta name="theme-color" content="#fafafa"> -->
 </head>
+
 <body>
